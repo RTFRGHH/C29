7e792a64371a5a308a74b5c0433117f1d2ef075e
RTFRGHH <86616571+RTFRGHH@users.noreply.github.com>
2021-06-28 08:37:14 -0700

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,25 @@
+class Sling{
+    constructor(bodyA,pointB){
+        var op={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,length:1
+
+        }
+        this.s=Matter.Constraint.create(op)
+        this.pointB=pointB;
+        Matter.World.add(world,this.s)
+
+
+    }
+    display(){
+        if(this.s.bodyA){
+            var pa=this.s.bodyA.position
+            var pb=this.pointB
+            line(pa.x,pa.y,pb.x,pb.y)
+        }
+    }
+    
+    
+
+}
